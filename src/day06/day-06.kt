33853156d896ef772c67b690867edcada57d9bd0
drigoor <>
2022-12-06 18:26:15 +0000

    day 6

---
+++ src/day06/day-06.kt
@@ -0,0 +1,72 @@
+package day06
+
+
+import utils.*
+
+
+val directory = object {}.javaClass.packageName // from: https://www.techiedelight.com/get-name-current-function-kotlin/
+
+
+fun myContains(str: String, start: Int, end: Int): Int {
+    for (i in start until end) {
+        val subStr = str.substring(i + 1, end)
+        if (subStr.contains(str[i])) {
+            return -1
+        }
+    }
+
+    return end
+}
+
+
+fun findMarker(str: String, markerSize: Int): Int {
+    var start = 0
+    var end = markerSize
+
+    while (end < str.length) {
+        val marker = myContains(str, start, end)
+
+        if (marker == -1) {
+            start++
+            end++
+        } else {
+            return marker
+        }
+    }
+
+    return -1
+}
+
+
+fun main() {
+    val lines = readLines(directory, "my.in")
+
+    val part1 = lines
+        .map {
+            findMarker(it, 4)
+        }
+
+    println(part1)
+
+    val part2 = lines
+        .map {
+            findMarker(it, 14)
+        }
+
+    println(part2)
+
+////    val str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+//    val str = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+//
+//    println(findMarker(str))
+//
+//
+////    println(lines)
+//
+//
+//    val part1 = 42
+//
+//    val part2 = 42
+//
+//    printResults("", part1, part2)
+}
---
+++ src/day07/day-07.kt
@@ -0,0 +1,20 @@
+package day07
+
+
+import utils.*
+
+
+val directory = object {}.javaClass.packageName // from: https://www.techiedelight.com/get-name-current-function-kotlin/
+
+
+fun main() {
+    val lines = readLines(directory, "example.in")
+
+    // HERE
+
+    val part1 = 42
+
+    val part2 = 42
+
+    printResults("", part1, part2)
+}
