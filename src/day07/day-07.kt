9b33d5fbd2d3a439f58ecaf79182f086a8f7587a
drigoor <>
2022-12-08 01:51:24 +0000

    day 7 done

--- src/day07/day-07.kt
+++ src/day07/day-07.kt
@@ -4,62 +4,119 @@ package day07
 import utils.*
 
 
-val directory = object {}.javaClass.packageName // from: https://www.techiedelight.com/get-name-current-function-kotlin/
+val directory: String = object {}.javaClass.packageName // from: https://www.techiedelight.com/get-name-current-function-kotlin/
 
 
-abstract class yCommon {
-    abstract var size: Int
-    abstract var name: String
+enum class ThingType {
+    DIR,
+    FILE
 }
 
-data class yFile(
-    override var size: Int = 0,
-    override var name: String = "",
-) : yCommon()
 
-data class yDir(
-    override var size: Int = 0,
-    override var name: String = "",
-    var next: yDir? = null,
-    var previous: yDir? = null,
-    val files: List<yFile> = listOf()
-) : yCommon()
+fun thingTypeToString(thing: Thing) = when (thing.type) {
+    ThingType.DIR -> "(dir, size=${thing.size}))"
+    ThingType.FILE -> "(file, size=${thing.size})"
+}
+
+
+fun thingToString(thing: Thing, indentation: Int = 0): String {
+    val result = StringBuilder()
+    val ln = System.lineSeparator()
+
+    result
+        .append("${" ".repeat(indentation)}- ${thing.name} ${thingTypeToString(thing)}")
+        .append(if (thing.contents.isEmpty()) "" else ln)
+        .append(thing.contents.joinToString(separator = ln, transform = { thingToString(it, indentation + 2) }))
+
+    return result.toString()
+}
+
+
+data class Thing(val type: ThingType, var name: String, var size: Int) {
+    lateinit var root: Thing
+    val contents: MutableList<Thing> = mutableListOf() // this keeps the order between directories and files (not great)
+
+    override fun toString() = thingToString(this)
+}
+
+
+fun dir(name: String) = Thing(ThingType.DIR, name, 0)
+
+
+fun file(name: String, size: Int) = Thing(ThingType.FILE, name, size)
+
+
+fun changeDirectory(current: Thing, name: String) =
+    current.contents
+        .filter { it.type == ThingType.DIR }
+        .find { it.name == name } ?: current // if the directory does not exist, it remains in the currentDir
+
+
+fun calculateAndUpdateTotalSize(current: Thing): Int {
+    if (current.size == 0) {
+        current.size = current.contents.fold(0) { total, next -> total + calculateAndUpdateTotalSize(next) }
+    }
+    return current.size
+}
 
 
 fun main() {
-    val lines = readLines(directory, "example.in")
+    val lines = readLines(directory, "my.in")
 
-    val roor = yDir(name = "/")
+    val root = dir("/")
+        .also { it.root = it } // this will allow to 'cd ..' in '/', which will become '/'
 
-    lateinit var currentDir: yDir
+    var currentDir = root
+    val dirs = mutableListOf(root)
 
     lines.forEach {
         val elements = it.split(" ")
 
         when (elements[0]) {
-            "$" -> {
-                if (elements[1] == "cd") {
-                    if (currentDir == null) {
-                        currentDir = yDir()
-                        currentDir.name = elements[2]
-
-                    } else {
-                    }
-                }
+            "$" ->
                 when (elements[1]) {
-                    "cd" -> {
-
-                    }
+                    "cd" ->
+                        when (elements[2]) {
+                            "/" -> currentDir = root
+                            ".." -> currentDir = currentDir.root
+                            else -> currentDir = changeDirectory(currentDir, elements[2])
+                        }
                 }
+
+            "dir" -> {
+                val newDir = dir(elements[1])
+                    .also { it.root = currentDir }
+
+                currentDir.contents += newDir
+                dirs += newDir
+            }
+
+            else -> {
+                val fileSize = elements[0].toInt()
+                currentDir.contents += file(elements[1], fileSize)
             }
         }
-
     }
 
+    calculateAndUpdateTotalSize(root)
+    /*
+        "e" 584
+        "a" 94853
+        "d" 24933642
+        "/" 48381165
+    */
 
-    val part1 = 42
+    val part1 = dirs.fold(0) { total, next -> total + if (next.size < 100000) next.size else 0 }
 
-    val part2 = 42
+    val totalSpace = 70000000
+    val usedSpace = root.size
+    val freeSpace = totalSpace - usedSpace
 
-    printResults("", part1, part2)
+    val desiredSpaceToFree = 30000000 - freeSpace
+
+    dirs.sortBy { it.size }
+
+    val part2 = dirs.find { it.size > desiredSpaceToFree }!!.size
+
+    printResults("1427048", part1, part2) // part2 --> 2940614
 }
