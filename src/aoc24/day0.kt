6df55629163b2b9bb5730e8e5102107b7be1aacf
drigoor <>
2024-12-03 17:50:50 +0000

    2024

---
+++ src/aoc24/day0.kt
@@ -0,0 +1,31 @@
+package aoc24
+
+
+fun main() {
+    val input = input(2024, 0)
+
+
+    fun part1(input: List<String>): Int {
+        return -1
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return -1
+    }
+
+
+    "--- 1 ---------------------------".println()
+
+    val raw1 = ""
+    val example1 = raw1.split('\n')
+    check(part1(example1) == 0)
+    part1(input).println() // TODO SOLUTION day 0 part 1 =
+
+
+    "--- 2 ---------------------------".println()
+
+    val example2 = raw1.split('\n')
+    check(part2(example2) == 0)
+    part2(input).println() // TODO SOLUTION day 0 part 2 =
+}
--- src/aoc24/utils.kt
+++ src/aoc24/utils.kt
@@ -1,4 +1,4 @@
-package aoc23
+package aoc24
 
 
 import okhttp3.OkHttpClient
@@ -19,30 +19,6 @@ fun List<String>.splitNoBlanks() = this.map { it.splitNoBlanks() }
 
 
 fun List<String>.splitNoBlanks2Int() = this.map { it.splitNoBlanks().map { it.toInt() } }
-
-
-// [0, 3, 6, 9, 12, 15] -> [3, 3, 3, 3, 3]
-fun List<Int>.reduceIt(): List<Int> {
-    var previous = this[0]
-    return this.subList(1, this.size).map {
-        val tmp = it - previous
-        previous = it
-        tmp
-    }
-}
-
-
-// [2, 0, 3, 10] --> 5 (which is 10-3-0-2 = 5)
-fun Iterable<Int>.subOfReversed(): Int {
-    var sub = 0
-    for (element in this.reversed()) {
-        sub = element - sub
-    }
-    return sub
-}
-
-
-fun List<Int>.allZeros() = this.all { it == 0 }
 
 
 private val client = OkHttpClient()
@@ -74,20 +50,4 @@ fun input(year: Int, day: Int): List<String> {
     }
 
     return f.readLines()
-}
-
-
-fun inputAndGroup(year: Int, day: Int): List<String> {
-    val dir = "resources/$year/"
-    File(dir).mkdirs()
-
-    val f = File("$dir/${"%02d".format(day)}.txt") // converts 1 -> 01
-    if (!f.exists()) {
-        f.createNewFile()
-        f.printWriter().use { out ->
-            out.print(fetch(year, day))
-        }
-    }
-
-    return f.readText().split("\n\n")
 }
